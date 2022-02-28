5c38ff0050693a584f86938dd3a98d0c4637a9e1
Caio Neves <ncaio037@gmail.com>
2022-02-28 18:26:30 -0300

    novo exercício

---
+++ Exercícios/5desafios/desafio1/códig.js
@@ -0,0 +1,12 @@
+function deslizar() {
+    var red = document.getElementById('quadrado')
+    red.style.backgroundColor = "red"
+}
+function sair() {
+    var purple = document.getElementById('quadrado')
+    purple.style.backgroundColor = "rgb(0, 89, 255)"
+}
+function clicar() {
+    var green = document.getElementById('quadrado')
+    green.style.backgroundColor = "green"
+}
